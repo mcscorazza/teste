36786ba85e5d80975c263e48d1f471504644e35e
mcscorazza <mcs.corazza@gmail.com>
2023-11-06 15:05:32 -0300

    ..

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>..::TESTE::..</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div>
+    <div class="box">
         TESTE
     </div>
 </body>
